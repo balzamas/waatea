76b669172a0dfcab93a7b3fc56cafa14b75289bb
David Berger <d.berger@dontsniff.co.uk>
2020-09-10 16:05:17 +0200

    Admin filtering

--- waateaapp/models.py
+++ waateaapp/models.py
@@ -57,7 +57,7 @@ class Gameday(MotherModel):
     ordering = ['date']
 
     def __str__(self):
-        return f"{self.date} - {self.season} - {self.games}"
+        return f"{self.date} - {self.season}"
 
 class Availbility(MotherModel):
     STATE_CHOICES=[
