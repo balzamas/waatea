c707370852cb73ce3718251fc85f43cd4baa823e
David Berger <d.berger@dontsniff.co.uk>
2020-09-10 16:05:17 +0200

    Admin filtering

--- waateaapp/admin.py
+++ waateaapp/admin.py
@@ -3,10 +3,28 @@ from django.contrib import admin
 from .models import Club, Team, Availbility, Season, Game, Gameday
 
 # Register your models here.
-# Register your models here.
 admin.site.register(Club)
 admin.site.register(Team)
-admin.site.register(Availbility)
 admin.site.register(Season)
-admin.site.register(Game)
-admin.site.register(Gameday)
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['home', 'away', 'team', 'season']
+    ordering = ['team']
+    list_filter = ('season', 'team')
+
+admin.site.register(Game, GameAdmin)
+
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ['player', 'gameday', 'state']
+    ordering = ['gameday__date', 'player', 'state']
+    list_filter = ('player', 'gameday', 'state')
+
+admin.site.register(Availbility, AvailabilityAdmin)
+
+
+class GamedayAdmin(admin.ModelAdmin):
+    list_display = ['date', 'season']
+    ordering = ['season', 'date']
+    list_filter = ('season', 'date')
+
+admin.site.register(Gameday, GamedayAdmin)
