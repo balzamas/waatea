5c29a78a0c2f4a09b21bf4b3a68641b80810cc62
David Berger <d.berger@dontsniff.co.uk>
2020-09-23 19:05:59 +0200

    ico

--- config/settings/production.py
+++ config/settings/production.py
@@ -155,8 +155,4 @@ LOGGING = {
 # ------------------------------------------------------------------------------
 SENDGRID_API_KEY = env("SENDGRID_API_KEY")
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
