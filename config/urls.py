80e942135d3c0c894b5e17f92887e86d23c3fb7c
David Berger <d.berger@dontsniff.co.uk>
2020-09-23 20:43:20 +0200

    ico

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", views.base.as_view(), name="home"),
     path(
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
