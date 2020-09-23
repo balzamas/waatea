426954eaf031718008fb00d1cf4905668a92acc5
David Berger <d.berger@dontsniff.co.uk>
2020-09-23 18:28:08 +0200

    ico

--- config/settings/production.py
+++ config/settings/production.py
@@ -94,7 +94,7 @@ ADMIN_URL = env("DJANGO_ADMIN_URL")
 # Anymail
 # ------------------------------------------------------------------------------
 # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-INSTALLED_APPS += ["sendgrid-django"]  # noqa F405
+INSTALLED_APPS += []  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
 # https://anymail.readthedocs.io/en/stable/esps/mailgun/
